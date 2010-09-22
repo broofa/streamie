eefd96032931d494e07862194d1929db7d95f36e
Robert Kieffer <robert@broofa.com>
2010-09-22 04:57:44 -0700

    wip

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var sys = require('sys'),
     http = require('http');
 
+var port = process.env.PORT || 8001;
+
 http.createServer(function (req, res) {
        setTimeout(function () {
           res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -8,6 +10,6 @@ http.createServer(function (req, res) {
           res.write('Hello World');
           res.close();
        }, 2000);
-}).listen(process.env["SERVER_PORT"] || 8000);
+}).listen(port);
 
-sys.puts('Server running at http://127.0.0.1:8000/');
+sys.puts('Server running at http://127.0.0.1:' + port + '/');
