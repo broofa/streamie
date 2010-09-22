4b95741367a790f12c1d9a3c2737db5af406433c
Robert Kieffer <rkieffer@dev101.snc1.facebook.com>
2010-09-22 11:39:19 -0700

    IE8 works. IE9 doesn't

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var url = require('url');
 var COUNT = 4;
 
 // Get port to run on
-var port = parseInt(process.env.PORT) || 8088;
+var port = parseInt(process.env.PORT) || 8085;
 
 // Log URL to hit
 sys.log('URL: http://localhost:' + port);
@@ -43,7 +43,7 @@ http.ServerResponse.prototype.sendPage = function(html, tokens) {
  */
 http.ServerResponse.prototype.writeMessage = function(msg, format) {
   msg = typeof(msg) == 'string' ? msg : JSON.stringify(msg);
-  if (format == 'xhr') {
+  if (format == 'json') {
     this.write(msg + '\n');
   } else {
     this.write('<script>process(' + msg + ');</script>\n');
@@ -56,20 +56,29 @@ http.ServerResponse.prototype.writeMessage = function(msg, format) {
 
 http.createServer(function(req, res) {
   var parts = url.parse(req.url, true);
-
+  sys.log(req.method + ' ' + parts.pathname);
   switch (parts.pathname) {
     // The endpoint for streaming data
     case '/stream':
-      var format = parts.query && parts.query.format;
-      if (format == 'xhr') {
+      var format = (parts.query && parts.query.format) || 'frame';
+      if (format == 'json') {
         // XHR/XDR stream is just \n-separated JSON objects, with no prelude.
         // We use the octet-stream type to work around Chrome's caching bug.
         // See http://code.google.com/p/chromium/issues/detail?id=2016#c41
-        res.writeHead(200, {
+        var headers = {
           'Cache-Control': 'no-cache',
           'Content-Type': 'application/octet-stream',
           'Transfer-Encoding': 'chunked'
-        });
+        };
+
+        // CORS support
+        if (req.headers.origin) {
+          headers['Access-Control-Allow-Origin'] = req.headers.origin;
+        }
+
+        res.writeHead(200, headers);
+        // Send 2Kb prelude as per http://blogs.msdn.com/b/ieinternals/archive/2010/04/06/comet-streaming-in-internet-explorer-with-xmlhttprequest-and-xdomainrequest.aspx
+        res.write(new Array(2048).join(' ') + '\n');
       } else {
         // Frame / htmlfile stream isa series of JSONP calls, which requires
         // some upfront script and prelude text
@@ -80,7 +89,7 @@ http.createServer(function(req, res) {
         });
 
         // Write prelude content
-        var prelude = fs.readFileSync('stream_prelude.html', 'utf8');
+        var prelude = fs.readFileSync('html/stream_prelude.html', 'utf8');
         prelude = prelude.replace(/SERVER_TIME/, new Date().getTime());
         res.write(prelude);
       }
@@ -94,11 +103,13 @@ http.createServer(function(req, res) {
 
         if (count-- > 0) {
           // Write a message and repeat in a few seconds
+          sys.log('sending ' + format + ' message');
           res.writeMessage(msg, format);
           setTimeout(send, 2e3);
         } else {
+          sys.log('done');
           // All done
-          if (format != 'xhr') {
+          if (format != 'json') {
             res.write('<p>-- Fin --</p></body></html>');
           }
           res.end();
@@ -116,9 +127,10 @@ http.createServer(function(req, res) {
 
     // Serve up static pages
     default:
-      var file = /\.html$/.test(req.url) ? req.url.replace(/\//, '') : 'index.html';
+      var file = /\.html$/.test(parts.pathname) ? parts.pathname.replace(/\//, '') : 'index.html';
       try {
-        res.sendPage(fs.readFileSync(file, 'utf8'), {SERVER_TIME: new Date().getTime()});
+        res.sendPage(fs.readFileSync('html/' + file, 'utf8'),
+                     {SERVER_TIME: new Date().getTime()});
       } catch (e) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.write(sys.inspect(e));
