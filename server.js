727e81203d4859e52f0ffc7b9633f12d289bc184
Robert Kieffer <robert@broofa.com>
2010-09-22 05:06:02 -0700

    port tweak

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var sys = require('sys'),
     http = require('http');
 
-var port = process.env.PORT || 8001;
+var port = parseInt(process.env.PORT) ||
+           parseInt(process.env.SERVER_PORT) ||
+           8001;
 
 http.createServer(function (req, res) {
-       setTimeout(function () {
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          sys.puts("serving Hello");
-          res.write('Hello World');
-          res.close();
-       }, 2000);
+  setTimeout(function () {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    sys.puts("serving Hello");
+    res.write('Hello World');
+    res.close();
+  }, 2000);
 }).listen(port);
 
 sys.puts('Server running at http://127.0.0.1:' + port + '/');
